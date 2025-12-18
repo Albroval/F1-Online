696d81ed1e943155928f7431434b70a5d0ea107c
Albroval <albroval@gmail.com>
2025-12-18 15:41:02 +0100

    Update 11 server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: "*" } });
 
 app.use(express.static(__dirname));
 
@@ -12,18 +12,15 @@ let rooms = {};
 io.on('connection', (socket) => {
     socket.on('join-room', (roomId) => {
         socket.join(roomId);
-        if (!rooms[roomId]) {
-            rooms[roomId] = { players: {}, readyCount: 0, started: false };
-        }
-        // Asignar datos iniciales al jugador
+        if (!rooms[roomId]) rooms[roomId] = { players: {}, readyCount: 0, started: false };
+        
         rooms[roomId].players[socket.id] = { 
             id: socket.id, 
             num: Object.keys(rooms[roomId].players).length + 1,
             laps: 0 
         };
-        socket.emit('assign-number', rooms[roomId].players[socket.id].num);
         
-        // Notificar a todos en la sala del nuevo jugador
+        socket.emit('assign-number', rooms[roomId].players[socket.id].num);
         io.to(roomId).emit('update-lobby', {
             x: Object.keys(rooms[roomId].players).length,
             y: rooms[roomId].readyCount
@@ -36,12 +33,11 @@ io.on('connection', (socket) => {
             let total = Object.keys(rooms[roomId].players).length;
             io.to(roomId).emit('update-lobby', { x: total, y: rooms[roomId].readyCount });
             
-            if (rooms[roomId].readyCount === total && total > 0) {
+            if (rooms[roomId].readyCount >= total && total > 0) {
                 rooms[roomId].started = true;
                 io.to(roomId).emit('init-race');
-                // Secuencia de semáforo
                 [1,2,3,4,5].forEach(n => setTimeout(() => io.to(roomId).emit('light', n), n * 1000));
-                setTimeout(() => io.to(roomId).emit('start-go'), 6500);
+                setTimeout(() => io.to(roomId).emit('start-go'), 6000);
             }
         }
     });
@@ -57,17 +53,17 @@ io.on('connection', (socket) => {
         }
     });
 
-    socket.on('disconnecting', () => {
-        socket.rooms.forEach(roomId => {
-            if (rooms[roomId] && rooms[roomId].players[socket.id]) {
+    socket.on('disconnect', () => {
+        for (let roomId in rooms) {
+            if (rooms[roomId].players[socket.id]) {
                 delete rooms[roomId].players[socket.id];
                 io.to(roomId).emit('update-lobby', {
                     x: Object.keys(rooms[roomId].players).length,
                     y: rooms[roomId].readyCount
                 });
             }
-        });
+        }
     });
 });
 
-server.listen(10000, () => console.log('Servidor F1 Activo'));
+server.listen(process.env.PORT || 10000);
